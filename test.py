0d1f65f17aede6564b148c1e4d946b4acba81638
Arjun G. Menon <arjungmenon@gmail.com>
2019-04-23 20:28:00 +0400

    test.py: extract markdown processing into function

--- test.py
+++ test.py
@@ -44,36 +44,33 @@ print(root)
 #     # for filename in filenames:
 #     #     print(os.path.join(dirpath, filename))
 
-with open('simple.md') as f:
-    text = f.read()
-
-md = markdown.Markdown(extensions = ['meta'])
-
-html = md.convert(text)
-# print html
-
-yaml_frontmatter = str()
-
-for name, lines in md.Meta.items():
-    yaml_frontmatter += '%s : %s \n' % (name, lines[0])
-    for line in lines[1:]:
-        yaml_frontmatter += ' ' * ( len(name) + 3 ) + line + '\n'
-
-print(yaml_frontmatter)
-
-yaml_metadata = yaml.load(yaml_frontmatter)
-
-print(yaml_metadata)
-
 class Metadata(object):
     def __init__(self, metadata_dict):
-        self.metadata_dict = metadata_dict
         for k, v in metadata_dict.items():
             self.__dict__[k] = v
     def __repr__(self):
-        return '\n'.join('%s : %s' % (k, v) for k, v in yaml_metadata.items())
+        return '\n'.join('%s : %s' % (k, v) for k, v in self.__dict__.items())
 
-metadata = Metadata(yaml_metadata)
+
+def process_markdown(md_filename):
+    with open(md_filename) as f:
+        text = f.read()
+
+    md = markdown.Markdown(extensions = ['meta'])
+    html = md.convert(text)
+    yaml_front_matter = ''
+
+    for name, lines in md.Meta.items():
+        yaml_front_matter += '%s : %s \n' % (name, lines[0])
+        for line in lines[1:]:
+            yaml_front_matter += ' ' * ( len(name) + 3 ) + line + '\n'
+
+    yaml_metadata = yaml.load(yaml_front_matter)
+    metadata = Metadata(yaml_metadata)
+    return metadata, html
+
+
+metadata, html = process_markdown('simple.md')
 
 print() 
 print(metadata)
