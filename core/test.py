4a18098b220f1aa34f6d1cf73ba397b6588591b4
Arjun G. Menon <contact@arjungmenon.com>
2022-07-24 05:35:33 -0400

    Rm watchdog

--- core/test.py
+++ core/test.py
@@ -1,6 +1,3 @@
-from watchdog.observers import Observer  # pyre-ignore
-from watchdog.events import LoggingEventHandler  # pyre-ignore
-
 from core.engine import *
 
 
@@ -28,28 +25,6 @@ def testMarkdownProcessing() -> None:
     print(metadata)
 
 
-def testChangeDetectionMonitoring() -> None:
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-    path = sys.argv[1] if len(sys.argv) > 1 else "."
-    eventHandler = LoggingEventHandler()  # type: ignore
-    observer = Observer()
-    observer.schedule(eventHandler, path, recursive=True)
-    observer.start()
-    # try:
-    #     while True:
-    #         time.sleep(1)
-    # except KeyboardInterrupt:
-    #     observer.stop()
-    time.sleep(1)
-    observer.stop()
-    observer.join()
-
-
 def run():
     testEngineProcessing()
     # testMarkdownProcessing()
-    # testChangeDetectionMonitoring()
