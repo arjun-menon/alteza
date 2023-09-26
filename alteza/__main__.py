103301426f0f8fd4fe43a92b718931c1e2011349
Arjun G. Menon <contact@arjungmenon.com>
2023-09-26 21:33:47 +0400

    Fix type

--- alteza/__main__.py
+++ alteza/__main__.py
@@ -4,11 +4,11 @@ import sys
 from .engine import run, Args
 
 
-def main():
+def main() -> None:
     run(Args().parse_args())
 
 
 # See: https://chriswarrick.com/blog/2014/09/15/python-apps-the-right-way-entry_points-and-scripts/
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
