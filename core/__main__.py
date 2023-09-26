f4939aefd358d2a85c590cb29be76df11a380b55
Arjun G. Menon <contact@arjungmenon.com>
2023-09-26 20:21:18 +0400

    Minor core/__main__.py update

--- core/__main__.py
+++ core/__main__.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
-from core.engine import run, Args
+import sys
 
-run(Args().parse_args())
+from .engine import run, Args
+
+
+def main():
+    run(Args().parse_args())
+
+
+# See: https://chriswarrick.com/blog/2014/09/15/python-apps-the-right-way-entry_points-and-scripts/
+
+if __name__ == "__main__":
+    sys.exit(main())
