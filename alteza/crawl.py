933dacb0380da804ca288e2b7b4261a023c5c372
Arjun G. Menon <contact@arjungmenon.com>
2025-06-30 02:53:53 -0400

    Turn off tqdm is non-interactive terminal (like GitHub actions)...

--- alteza/crawl.py
+++ alteza/crawl.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from collections import defaultdict
 from dataclasses import dataclass
 from datetime import datetime
@@ -14,18 +15,21 @@ class ProgressBar:
 	@classmethod
 	def start(cls, total: int, desc: str = '') -> None:
 		assert cls.pbar is None
+		if not sys.stdin.isatty():
+			print(f'{desc}...')
+			return
 		cls.pbar = tqdm(total=total, desc=desc)
 
 	@classmethod
 	def update(cls, n: int) -> None:
-		assert cls.pbar is not None
-		cls.pbar.update(n)
+		if cls.pbar is not None:
+			cls.pbar.update(n)
 
 	@classmethod
 	def write(cls, *args: Any, sep: str = ' ', end: str = '\n') -> None:
-		# if cls.pbar is None:
-		# 	print(*args, sep=sep, end=end)
-		# 	return
+		if cls.pbar is None:
+			print(*args, sep=sep, end=end)
+			return
 		message = sep.join(str(arg) for arg in args)
 		assert cls.pbar is not None
 		cls.pbar.write(message, end=end)
