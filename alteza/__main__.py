3889e11ae47100f0bd1c1f6c04c52a696c9c082e
Arjun G. Menon <contact@arjungmenon.com>
2025-06-30 03:07:23 -0400

    Print error code exit message...

--- alteza/__main__.py
+++ alteza/__main__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import time
 from sys import exit
 from .driver import Driver, Args
 
@@ -10,4 +11,8 @@ def main() -> int:
 # See: https://chriswarrick.com/blog/2014/09/15/python-apps-the-right-way-entry_points-and-scripts/
 
 if __name__ == '__main__':
-	exit(main())
+	rc = main()
+	if rc != 0:
+		print(f'Exiting with error code {rc}...')
+		time.sleep(3)
+		exit(rc)
